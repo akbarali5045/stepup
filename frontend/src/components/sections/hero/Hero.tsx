ab326498882f18f3633a3b1b957114a8616039ec
Akbar Ali <ak4akbarali@gmail.com>
2026-07-16 15:53:07 +0500

    Create hero banner with Swiper slider

--- frontend/src/components/sections/hero/Hero.tsx
+++ frontend/src/components/sections/hero/Hero.tsx
@@ -1,13 +1,46 @@
+"use client";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/pagination";
+
 import HeroCard from "./HeroCard";
 import { heroSlides } from "@/data/heroData";
 
+
 export default function Hero() {
+
   return (
     <section className="w-full">
-      <HeroCard
-        image={heroSlides[0].image}
-        alt={heroSlides[0].alt}
-      />
+
+      <Swiper
+  modules={[Autoplay, Pagination]}
+  autoplay={{
+    delay: 2000,
+  }}
+  loop={true}
+  pagination={{
+    clickable: true,
+  }}
+>
+
+        {
+          heroSlides.map((slide) => (
+            <SwiperSlide key={slide.id}>
+
+              <HeroCard
+                image={slide.image}
+                alt={slide.alt}
+              />
+
+            </SwiperSlide>
+          ))
+        }
+
+      </Swiper>
+
     </section>
   );
 }
--- frontend/src/data/heroData.ts
+++ frontend/src/data/heroData.ts
@@ -2,11 +2,12 @@ export const heroSlides = [
   {
     id: 1,
     image: "/images/hero/Banner1.webp",
-    alt: "New Collection Banner",
+    alt: "Banner 1",
   },
   {
     id: 2,
-    image: "/images/hero/Banner.webp",
-    alt: "Summer Sale Banner",
+    image: "/images/hero/Banner2.webp",
+    alt: "Banner 2",
   },
+  
 ];
